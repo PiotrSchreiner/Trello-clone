95fc46d3574ded73d84c5cc90f238c1568177704
Piotr Schreiner <piotrschreiner@gmail.com>
2023-07-17 02:16:27 +0200

    Implemented zustand | Setted up the type def.

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,11 +1,22 @@
-import { DragDropContext } from 'react-beautiful-dnd';
+'use client'
+
+import { useEffect } from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 function Board() {
+useEffect(() => {
+
+}, [])
+
   return (
-    <div>
-      
-    </div>
-  )
+    <DragDropContext>
+      <Droppable droppableId='board' direction='horizontal' type='column'>
+        {(provided) => (
+          <div></div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 }
 
-export default Board
+export default Board;
