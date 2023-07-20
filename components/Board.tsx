588815b615803df00911a683436e1162532c027a
Piotr Schreiner <piotrschreiner@gmail.com>
2023-07-20 18:38:05 +0200

    implemented todosGroupedByColumn

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,21 +1,24 @@
 'use client'
 
+import { useBoardStore } from '@/store/BoardStore';
 import { useEffect } from 'react';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 function Board() {
-useEffect(() => {
+  const getBoard = useBoardStore((state) => state.getBoard)
 
-}, [])
+useEffect(() => {
+   getBoard();
+}, [getBoard])
 
   return (
-   {/* <DragDropContext>
+    <DragDropContext>
       <Droppable droppableId='board' direction='horizontal' type='column'>
         {(provided) => (
           <div></div>
         )}
       </Droppable>
-    </DragDropContext>*/}
+    </DragDropContext>
   );
 }
 
