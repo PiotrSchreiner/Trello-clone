dd6f08aad13a760c2393b78336589945ef9b7db2
Piotr Schreiner <piotrschreiner@gmail.com>
2023-07-20 20:11:59 +0200

    fiexed the board issue

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import React from 'react'
 import {MagnifyingGlassIcon, UserCircleIcon} from "@heroicons/react/24/solid";
 import Avatar from 'react-avatar';
-
+// flex
 function Header() {
   return (
     <header>
