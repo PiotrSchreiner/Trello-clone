a7d0e34e994e5400a6518126c54ca38168b50815
Piotr Schreiner <piotrschreiner@gmail.com>
2023-07-25 00:39:49 +0200

    Implementing the search functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,13 @@ import Image from 'next/image'
 import React from 'react'
 import {MagnifyingGlassIcon, UserCircleIcon} from "@heroicons/react/24/solid";
 import Avatar from 'react-avatar';
+import { useBoardStore } from '@/store/BoardStore';
 // flex
 function Header() {
+  const [searchString, setSearchString] = useBoardStore((state) => [
+    state.searchString,
+    state.setSearchString,
+  ])
   return (
     <header>
       <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl'>
@@ -38,7 +43,7 @@ function Header() {
       <div className='flex items-center space-x-5 flex-1 justify-end w-full'>
         <form className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
           <MagnifyingGlassIcon className='h-6 w-6 text-gray-400' />
-          <input type="text" placeholder='Search' className='flex-1 outline-none'/>
+          <input type="text" placeholder='Search' value={searchString} onChange={(e) => setSearchString(e.target.value)} className='flex-1 outline-none'/>
           <button type='submit' hidden>Search</button>
         </form>
         <Avatar name='Pioti' round size='50' color='#0055D1'/>
